cb522095068cae86b47eaf973410686d974b5983
ghhhjjjhfvhbhbh-bit <ghhhjjjhfvhbhbh-bit@users.noreply.github.com>
2026-09-15 12:29:20 +0500

    point admin panel API to deployed Railway backend

--- adminpanel/src/services/api.js
+++ adminpanel/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = '/api';
+const API_BASE = 'https://hearty-learning-production-d991.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_BASE,
